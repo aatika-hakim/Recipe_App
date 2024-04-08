6d8c941a5aff090cf99af1552f129d220876d589
Aatika hakim <aatikahakim90@gmail.com>
2024-04-08 07:07:18 +0500

    edit

--- componenets/layout/Footer.js
+++ componenets/layout/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <h1 className={styles.title}>Food Gala</h1>
-      <Text>Find the perfect meal recipe for you</Text>
+      <Text className='text'>Find the perfect meal recipe for you</Text>
       <Text className={styles.copyright}>
-        © “My-Meals” 2022 All right reserved.
+        © “Food Gala” 2022 All right reserved.
       </Text>
     </footer>
   )
