83359a8cc5232ce49e77d8616a59c4e1d7166e3b
Aatika hakim <aatikahakim90@gmail.com>
2024-05-13 10:30:33 +0500

    text added

--- componenets/layout/Footer.js
+++ componenets/layout/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../../styles/Footer.module.scss'
 import Text from '../text/Text'
+import Link from 'next/link'
 
 function Footer() {
   return (
@@ -8,7 +9,7 @@ function Footer() {
       <h1 className={styles.title}>Food Gala</h1>
       <Text>Find the perfect meal recipe for you</Text>
       <Text className={styles.copyright}>
-        © “Food Gala” 2022 All right reserved. Designed and developed by <a href="http://aatikahakim90@gmail.com"  Aatika Hakim />
+        © “Food Gala” 2022 All right reserved. Designed and developed by <Link href="http://aatikahakim90@gmail.com"> Aatika Hakim </Link>
       </Text>
     </footer>
   )
